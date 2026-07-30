f6c5b771eede2d2a3d5799d9ac73b0dac7a278d6
Jairo Matías <jairo_1829@hotmail.com>
2026-07-30 18:56:02 -0400

    feat: agregar actualización de marcas

--- RentCarClient/src/app/services/marca.service.ts
+++ RentCarClient/src/app/services/marca.service.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// LÍNEA 4 BORRADA: Ya no intentamos importar 'Marca' de un lugar que no existe
-
 @Injectable({
   providedIn: 'root'
 })
 export class MarcaService {
   private apiUrl = 'http://localhost:5266/api/Marcas';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  // Usamos 'any[]' para evitar que Angular busque una interfaz que no encuentra
   getMarcas(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
   crearMarca(marca: any): Observable<any> {
     return this.http.post<any>(this.apiUrl, marca);
+  }
+
+  actualizarMarca(marca: any): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/${marca.id}`,
+      marca
+    );
   }
 
   deleteMarca(id: number): Observable<any> {
